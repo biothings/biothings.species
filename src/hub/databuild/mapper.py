68e4691f4d282a65e42f35dcafee95b60aa67755
Dylan Welzel <dylanwelzel@gmail.com>
2024-12-09 13:47:05 -0700

    move changes to mapper.py

--- src/hub/databuild/mapper.py
+++ src/hub/databuild/mapper.py
@@ -1,26 +1,32 @@
-import biothings, config
-biothings.config_for_app(config)
-from biothings.utils.common import loadobj
-import biothings.utils.mongo as mongo
+import logging
+from collections import defaultdict, deque
+
+import biothings
 import biothings.hub.databuild.mapper as mapper
+import biothings.utils.mongo as mongo
+import config
+from biothings.utils.common import loadobj
+
 # just to get the collection name
 from ..dataload.sources.geneinfo.uploader import GeneInfoUploader
 from ..dataload.sources.taxonomy.uploader import TaxonomyNodesUploader
+
+biothings.config_for_app(config)
 
 
 class HasGeneMapper(mapper.BaseMapper):
 
     def __init__(self, *args, **kwargs):
-        super(HasGeneMapper,self).__init__(*args,**kwargs)
+        super(HasGeneMapper, self).__init__(*args, **kwargs)
         self.cache = None
 
     def load(self):
         if self.cache is None:
             # this is a whole dict containing all taxonomu _ids
             col = mongo.get_src_db()[GeneInfoUploader.name]
-            self.cache = [d["_id"] for d in col.find({},{"_id":1})]
+            self.cache = [d["_id"] for d in col.find({}, {"_id": 1})]
 
-    def process(self,docs):
+    def process(self, docs):
         for doc in docs:
             if doc["_id"] in self.cache:
                 doc["has_gene"] = True
@@ -32,30 +38,78 @@ class HasGeneMapper(mapper.BaseMapper):
 class LineageMapper(mapper.BaseMapper):
 
     def __init__(self, *args, **kwargs):
-        super(LineageMapper,self).__init__(*args,**kwargs)
+        super(LineageMapper, self).__init__(*args, **kwargs)
         self.cache = None
+        self.parent_to_children = None
+        self.descendants_map = None
+        self.logger = logging.getLogger(__name__)
 
     def load(self):
         if self.cache is None:
             col = mongo.get_src_db()[TaxonomyNodesUploader.name]
             self.cache = {}
-            [self.cache.setdefault(d["taxid"],d["parent_taxid"]) for d in col.find({},{"parent_taxid":1,"taxid":1})]
+            # Build cache: taxid -> parent_taxid
+            for d in col.find({}, {"parent_taxid": 1, "taxid": 1}):
+                self.cache[d["taxid"]] = d["parent_taxid"]
 
-    def get_lineage(self,doc):
-        if doc['taxid'] == doc['parent_taxid']: #take care of node #1
-            # we reached the top of the taxonomy tree
+            # Build parent_to_children map
+            self.parent_to_children = defaultdict(list)
+            for taxid, parent_taxid in self.cache.items():
+                if taxid != parent_taxid:  # exclude root which points to itself
+                    self.parent_to_children[parent_taxid].append(taxid)
+
+            # Precompute descendants for all taxids
+            self.descendants_map = {}
+            all_taxids = list(self.cache.keys())
+            self.logger.info(
+                "Precomputing descendants for %d nodes..." % len(all_taxids))
+            for tid in all_taxids:
+                self.descendants_map[tid] = self._get_all_descendants(tid)
+            self.logger.info("Finished precomputing descendants.")
+
+    def _get_all_descendants(self, taxid):
+        # BFS or DFS to find all descendants
+        descendants = []
+        queue = deque(self.parent_to_children.get(taxid, []))
+        while queue:
+            child = queue.popleft()
+            descendants.append(child)
+            # add children of this child
+            for c in self.parent_to_children.get(child, []):
+                queue.append(c)
+        return descendants
+
+    def get_lineage(self, doc):
+        if doc['taxid'] == doc['parent_taxid']:
+            # We reached the top of the taxonomy tree
             doc['lineage'] = [doc['taxid']]
-            return doc
-        # initiate lineage with information we have in the current doc
-        lineage = [doc['taxid'], doc['parent_taxid']]
-        while lineage[-1] != 1:
-            parent = self.cache[lineage[-1]]
-            lineage.append(parent)
-        doc['lineage'] = lineage
+        else:
+            # initiate lineage with current doc
+            lineage = [doc['taxid'], doc['parent_taxid']]
+            while lineage[-1] != 1:
+                parent = self.cache[lineage[-1]]
+                lineage.append(parent)
+            doc['lineage'] = lineage
+
+        # Once lineage is computed, we can set the other fields:
+        # parents
+        if doc['taxid'] != doc['parent_taxid']:
+            doc['parents'] = [doc['parent_taxid']]
+        else:
+            doc['parents'] = []
+
+        # children
+        doc['children'] = self.parent_to_children.get(doc['taxid'], [])
+
+        # ancestors (lineage except the node itself)
+        doc['ancestors'] = doc['lineage'][1:]
+
+        # descendants from precomputed map
+        doc['descendants'] = self.descendants_map.get(doc['taxid'], [])
+
         return doc
 
-    def process(self,docs):
+    def process(self, docs):
         for doc in docs:
             doc = self.get_lineage(doc)
             yield doc
-
