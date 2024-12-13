aeb2377cb49ad0dc27f3257ea70c6eaacabee515
Dylan Welzel <dylanwelzel@gmail.com>
2024-12-12 17:31:37 -0700

    remove parents and descendants, update mapping

--- src/hub/databuild/mapper.py
+++ src/hub/databuild/mapper.py
@@ -1,11 +1,10 @@
 import logging
-from collections import defaultdict, deque
+from collections import defaultdict
 
 import biothings
 import biothings.hub.databuild.mapper as mapper
 import biothings.utils.mongo as mongo
 import config
-from biothings.utils.common import loadobj
 
 # just to get the collection name
 from ..dataload.sources.geneinfo.uploader import GeneInfoUploader
@@ -41,7 +40,6 @@ class LineageMapper(mapper.BaseMapper):
         super(LineageMapper, self).__init__(*args, **kwargs)
         self.cache = None
         self.parent_to_children = None
-        self.descendants_map = None
         self.logger = logging.getLogger(__name__)
 
     def load(self):
@@ -52,32 +50,10 @@ class LineageMapper(mapper.BaseMapper):
             for d in col.find({}, {"parent_taxid": 1, "taxid": 1}):
                 self.cache[d["taxid"]] = d["parent_taxid"]
 
-            # Build parent_to_children map
             self.parent_to_children = defaultdict(list)
             for taxid, parent_taxid in self.cache.items():
                 if taxid != parent_taxid:  # exclude root which points to itself
                     self.parent_to_children[parent_taxid].append(taxid)
-
-            # Precompute descendants for all taxids
-            self.descendants_map = {}
-            all_taxids = list(self.cache.keys())
-            self.logger.info(
-                "Precomputing descendants for %d nodes..." % len(all_taxids))
-            for tid in all_taxids:
-                self.descendants_map[tid] = self._get_all_descendants(tid)
-            self.logger.info("Finished precomputing descendants.")
-
-    def _get_all_descendants(self, taxid):
-        # BFS to find all descendants
-        descendants = []
-        queue = deque(self.parent_to_children.get(taxid, []))
-        while queue:
-            child = queue.popleft()
-            descendants.append(child)
-            # add children of this child
-            for c in self.parent_to_children.get(child, []):
-                queue.append(c)
-        return descendants
 
     def get_lineage(self, doc):
         if doc['taxid'] == doc['parent_taxid']:
@@ -91,13 +67,6 @@ class LineageMapper(mapper.BaseMapper):
                 lineage.append(parent)
             doc['lineage'] = lineage
 
-        # Once lineage is computed, we can set the other fields:
-        # parents
-        if doc['taxid'] != doc['parent_taxid']:
-            doc['parents'] = [doc['parent_taxid']]
-        else:
-            doc['parents'] = []
-
         # children
         doc['children'] = self.parent_to_children.get(doc['taxid'], [])
 
@@ -109,13 +78,6 @@ class LineageMapper(mapper.BaseMapper):
                     doc['taxid'], len(doc['ancestors']))
             )
             doc['ancestors'] = doc['ancestors'][:1000]
-
-        # descendants from precomputed map
-        doc['descendants'] = self.descendants_map.get(doc['taxid'], [])
-        if len(doc['descendants']) > 1000:
-            self.logger.warning(
-                "Descendants for taxid %s is over 1000: %d. Capping to 1000." % (doc['taxid'], len(doc['descendants'])))
-            doc['descendants'] = doc['descendants'][:1000]
 
         return doc
 
--- src/hub/dataindex/indexer.py
+++ src/hub/dataindex/indexer.py
@@ -4,4 +4,8 @@ import biothings.hub.dataindex.indexer as indexer
 class TaxonomyIndexer(indexer.Indexer):
     def __init__(self, build_doc, indexer_env, index_name):
         super().__init__(build_doc, indexer_env, index_name)
-        self.es_index_mappings['properties']['lineage'] = {'type': 'long'}
+        self.es_index_mappings['properties'].update({
+            'lineage': {'type': 'long'},
+            'children': {'type': 'long'},
+            'ancestors': {'type': 'long'}
+        })
