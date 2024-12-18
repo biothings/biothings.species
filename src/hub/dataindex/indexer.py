f527b8caf2114f85761c0ac01f047de0c892b845
Dylan Welzel <dylanwelzel@gmail.com>
2024-12-18 10:07:51 -0700

    update type to integer

--- src/hub/dataindex/indexer.py
+++ src/hub/dataindex/indexer.py
@@ -5,7 +5,7 @@ class TaxonomyIndexer(indexer.Indexer):
     def __init__(self, build_doc, indexer_env, index_name):
         super().__init__(build_doc, indexer_env, index_name)
         self.es_index_mappings['properties'].update({
-            'lineage': {'type': 'long'},
-            'children': {'type': 'long'},
-            'ancestors': {'type': 'long'}
+            'lineage': {'type': 'integer'},
+            'children': {'type': 'integer'},
+            'ancestors': {'type': 'integer'}
         })
