c7b2931015942c48cb5217a4ad4dde03017ef357
Dylan Welzel <dylanwelzel@gmail.com>
2025-01-14 16:10:08 -0700

    add separate list for children with has_gene

--- src/hub/databuild/mapper.py
+++ src/hub/databuild/mapper.py
@@ -34,19 +34,20 @@ class HasGeneMapper(mapper.BaseMapper):
             yield doc
 
 
-class LineageMapper(mapper.BaseMapper):
+class LineageMapper(BaseMapper):
 
     def __init__(self, *args, **kwargs):
         super(LineageMapper, self).__init__(*args, **kwargs)
         self.cache = None
         self.parent_to_children = None
         self.logger = logging.getLogger(__name__)
+        self.has_gene_set = None
 
     def load(self):
         if self.cache is None:
+            # Build self.cache: taxid -> parent_taxid
             col = mongo.get_src_db()[TaxonomyNodesUploader.name]
             self.cache = {}
-            # Build cache: taxid -> parent_taxid
             for d in col.find({}, {"parent_taxid": 1, "taxid": 1}):
                 self.cache[d["taxid"]] = d["parent_taxid"]
 
@@ -55,27 +56,41 @@ class LineageMapper(mapper.BaseMapper):
                 if taxid != parent_taxid:  # exclude root which points to itself
                     self.parent_to_children[parent_taxid].append(taxid)
 
+        # Also build self.has_gene_set
+        if self.has_gene_set is None:
+            geneinfo_col = mongo.get_src_db()[GeneInfoUploader.name]
+            self.has_gene_set = set(doc["_id"]
+                                    for doc in geneinfo_col.find({}, {"_id": 1}))
+
     def get_lineage(self, doc):
-        if doc['taxid'] == doc['parent_taxid']:
+        if doc["taxid"] == doc["parent_taxid"]:
             # We reached the top of the taxonomy tree
-            doc['lineage'] = [doc['taxid']]
+            doc["lineage"] = [doc["taxid"]]
         else:
             # initiate lineage with current doc
-            lineage = [doc['taxid'], doc['parent_taxid']]
+            lineage = [doc["taxid"], doc["parent_taxid"]]
             while lineage[-1] != 1:
                 parent = self.cache[lineage[-1]]
                 lineage.append(parent)
-            doc['lineage'] = lineage
+            doc["lineage"] = lineage
 
         # children
-        children = self.parent_to_children.get(doc['taxid'], [])
+        children = self.parent_to_children.get(doc["taxid"], [])
         if children:
-            doc['children'] = children
+            doc["children"] = children
+
+            # Build _has_gene_children as a filtered subset
+            doc["_has_gene_children"] = [
+                child for child in children if child in self.has_gene_set
+            ]
+        else:
+            doc["children"] = []
+            doc["_has_gene_children"] = []
 
         # ancestors (lineage except the node itself)
-        ancestors = doc['lineage'][1:]
+        ancestors = doc["lineage"][1:]
         if ancestors:
-            doc['ancestors'] = ancestors
+            doc["ancestors"] = ancestors
 
         return doc
 
