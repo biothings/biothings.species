a3c6cbffbea73c9125acbbd74a273747cd845c9d
Dylan Welzel <dylanwelzel@gmail.com>
2025-11-13 10:15:19 -0800

    keep original taxonomy names and update mapping for name keywords with normalizer

--- src/hub/dataload/sources/taxonomy/parser.py
+++ src/hub/dataload/sources/taxonomy/parser.py
@@ -1,14 +1,14 @@
-from itertools import groupby
 from collections import defaultdict
+from itertools import groupby
 
 
 def parse_refseq_names(names_file):
     '''
     names_file is a file-like object yielding 'names.dmp' from taxdump.tar.gz
     '''
-    #Collapse all the following fields into "synonyms"
-    other_names = ["acronym","anamorph","blast name","equivalent name","genbank acronym","genbank anamorph",
-    "genbank synonym","includes","misnomer","misspelling","synonym","teleomorph"]
+    # Collapse all the following fields into "synonyms"
+    other_names = ["acronym", "anamorph", "blast name", "equivalent name", "genbank acronym", "genbank anamorph",
+                   "genbank synonym", "includes", "misnomer", "misspelling", "synonym", "teleomorph"]
     # keep separate: "common name", "genbank common name"
     names_gb = groupby(names_file, lambda x: x[:x.index('\t')])
     for taxid, entry in names_gb:
@@ -18,24 +18,26 @@ def parse_refseq_names(names_file):
         for line in entry:
             split_line = line.split('\t')
             field = split_line[6]
-            value = split_line[2].lower()
+            value = split_line[2].strip()
             if field == 'scientific name':
-                d['scientific_name'] = value #only one per entry. Store as str (not in a list)
+                # only one per entry. Store as str (not in a list)
+                d['scientific_name'] = value
             elif field in other_names:
-                d['other_names'].append(value) #always a list
+                d['other_names'].append(value)  # always a list
             elif field == "common name":
-                if d['common_name'] == []: #empty
-                    d['common_name'] = value # set as string
-                elif type(d['common_name']) == str: # has a single entry
-                    d['common_name'] = [d['common_name']] #make it a list
+                if d['common_name'] == []:  # empty
+                    d['common_name'] = value  # set as string
+                elif type(d['common_name']) == str:  # has a single entry
+                    d['common_name'] = [d['common_name']]  # make it a list
                     d['common_name'].append(value)
                 else:
                     d['common_name'].append(value)
             elif field == "genbank common name":
-                if d['genbank_common_name'] == []: #empty
-                    d['genbank_common_name'] = value # set as string
-                elif type(d['genbank_common_name']) == str: # has a single entry
-                    d['genbank_common_name'] = [d['genbank_common_name']] #make it a list
+                if d['genbank_common_name'] == []:  # empty
+                    d['genbank_common_name'] = value  # set as string
+                elif type(d['genbank_common_name']) == str:  # has a single entry
+                    d['genbank_common_name'] = [
+                        d['genbank_common_name']]  # make it a list
                     d['genbank_common_name'].append(value)
                 else:
                     d['genbank_common_name'].append(value)
--- src/hub/dataload/sources/taxonomy/uploader.py
+++ src/hub/dataload/sources/taxonomy/uploader.py
@@ -1,6 +1,7 @@
 import os
 
 import biothings.hub.dataload.uploader as uploader
+
 from .parser import parse_refseq_names, parse_refseq_nodes
 
 
@@ -9,25 +10,25 @@ class TaxonomyNodesUploader(uploader.BaseSourceUploader):
     main_source = "taxonomy"
     name = "nodes"
 
-    def load_data(self,data_folder):
-        nodes_file = os.path.join(data_folder,"nodes.dmp")
+    def load_data(self, data_folder):
+        nodes_file = os.path.join(data_folder, "nodes.dmp")
         self.logger.info("Load data from file '%s'" % nodes_file)
         return parse_refseq_nodes(open(nodes_file))
 
     @classmethod
     def get_mapping(klass):
         return {
-                "rank": {
-                    "type": "text"
-                    },
-                "taxid": {
-                    "copy_to" : ["all"],
-                    "type": "long"
-                    },
-                "parent_taxid": {
-                    "type": "long"
-                    },
-                }
+            "rank": {
+                "type": "text"
+            },
+            "taxid": {
+                "copy_to": ["all"],
+                "type": "long"
+            },
+            "parent_taxid": {
+                "type": "long"
+            },
+        }
 
 
 class TaxonomyNamesUploader(uploader.BaseSourceUploader):
@@ -35,27 +36,44 @@ class TaxonomyNamesUploader(uploader.BaseSourceUploader):
     main_source = "taxonomy"
     name = "names"
     __metadata__ = {
-            "mapper" : 'has_gene',
-            }
+        "mapper": 'has_gene',
+    }
 
-    def load_data(self,data_folder):
-        names_file = os.path.join(data_folder,"names.dmp")
+    def load_data(self, data_folder):
+        names_file = os.path.join(data_folder, "names.dmp")
         self.logger.info("Load data from file '%s'" % names_file)
         return parse_refseq_names(open(names_file))
 
     @classmethod
     def get_mapping(klass):
         return {
-                "scientific_name": {
-                    "copy_to" : ["all"],
-                    "type": "text"
-                    },
-                "common_name": {
-                    "copy_to" : ["all"],
-                    "type": "text"
-                    },
-                "genbank_common_name": {
-                    "type": "text"
-                    }
+            "scientific_name": {
+                "copy_to": ["all"],
+                "type": "text",
+                "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "normalizer": "keyword_lowercase_normalizer"
+                        }
                 }
-
+            },
+            "common_name": {
+                "copy_to": ["all"],
+                "type": "text",
+                "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "normalizer": "keyword_lowercase_normalizer"
+                        }
+                }
+            },
+            "genbank_common_name": {
+                "type": "text",
+                "fields": {
+                        "keyword": {
+                            "type": "keyword",
+                            "normalizer": "keyword_lowercase_normalizer"
+                        }
+                }
+            }
+        }
